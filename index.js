652f0a5e8471d4e1463ead1f8e863fcad1150bb4
Xiejvar <jbg9993@gmail.com>
2020-06-17 12:54:36 +0200

    Primer commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let passengers = document.getElementById('passengersPax')
+let btnPassengers = document.getElementById('btn').addEventListener('click', passengersPerAge)
+
+
+function passengersPerAge() {
+    let passengersPax = passengers.value
+
+    renderPassengersDivs(passengersPax)
+}
+
+function renderPassengersDivs(passengersPax){
+    let quantity = passengersPax
+    let i = 0
+
+    for(i = 0; i < quantity; i++) {
+            console.log("iteraciones: " + i)
+            let newDiv = document.createElement('div')
+            newDiv.innerHTML = `<input type="number" id="passengerAge${i}" name="passengerAge"min="0" max="100">`
+            document.getElementById('passengersAge').appendChild(newDiv)    
+    } 
+    
+    return i
+}
+let hora = document.getElementById('arrivalAirport').value
+let search = document.getElementById('searchForm')
+ search.addEventListener('submit', (evt)=>{
+    console.log(hora)
+ evt.preventDefault()
+ })
