cb80c88865b2364debe7543c6c6d7893ae493e58
Xiejvar <jbg9993@gmail.com>
2020-06-18 17:25:19 +0200

    añadida funcion removeChild

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ let passengers = document.getElementById('passengersPax')
 let btnPassengers = document.getElementById('btn').addEventListener('click', passengersPerAge)
 
 
+
+function borrarChilds () {
+    var div = document.getElementById('passengersAge');
+while (div.firstChild) {
+    div.removeChild(div.firstChild);
+}
+}
+
 function passengersPerAge() {
     let passengersPax = passengers.value
-
+    borrarChilds()
     renderPassengersDivs(passengersPax)
 }
 
 function renderPassengersDivs(passengersPax){
     let quantity = passengersPax
-    let i = 0
-
+    let i = 0;
     for(i = 0; i < quantity; i++) {
             console.log("iteraciones: " + i)
             let newDiv = document.createElement('div')
             newDiv.innerHTML = `<input type="number" id="passengerAge${i}" name="passengerAge"min="0" max="100">`
-            document.getElementById('passengersAge').appendChild(newDiv)    
+            document.getElementById('passengersAge').appendChild(newDiv)
     } 
     
     return i
@@ -24,6 +31,8 @@ function renderPassengersDivs(passengersPax){
 let hora = document.getElementById('arrivalAirport').value
 let search = document.getElementById('searchForm')
  search.addEventListener('submit', (evt)=>{
+     
     console.log(hora)
  evt.preventDefault()
+ 
  })
